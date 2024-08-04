53eef2a3eb732bb80a89b585eed34110a5489ab6
brunormoura <brunormoura@gmail.com>
2024-08-04 16:29:37 -0300

    state:working

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Init App:',
+              'Init App',
             ),
           ],
         ),
